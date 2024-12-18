9df3ab150ee0ccbd2ef72cc463aa7268ce5f65df
Zee Why <hello_z33@proton.me>
2024-12-17 23:25:25 -0500

    build fixes lol

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import type { PluginAPI } from "tailwindcss/types/config";
 
 const config: Config = {
   content: [
@@ -30,7 +31,7 @@ const config: Config = {
     },
   },
   plugins: [
-    function ({ addUtilities }) {
+    function ({ addUtilities }: PluginAPI) {
       const utilities = {
         ".text-stroke": {
           "-webkit-text-stroke": "1px white",
